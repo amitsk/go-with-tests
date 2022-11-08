ee0ea56de9e1cc3bb8fe09efe4de904f7ed388b2
Amit K <amitsk1974@gmail.com>
2022-11-07 20:20:08 -0800

    concurrency

---
+++ concurrency/goroutines.go
@@ -0,0 +1,47 @@
+package concurrency
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func Goroutines() {
+	// Allocate 1 logical processor for the scheduler to use.
+	runtime.GOMAXPROCS(1)
+
+	// wg is used to wait for the program to finish.
+	// Add a count of two, one for each goroutine.
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	fmt.Println("Start Goroutines")
+
+	// Declare an anonymous function and create a goroutine.
+	go func() {
+		defer wg.Done()
+
+		// Display the alphabet three times
+		for count := 0; count < 3; count++ {
+			for char := 'a'; char < 'a'+26; char++ {
+				fmt.Printf("%c ", char)
+			}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		// Display the alphabet three times
+		for count := 0; count < 3; count++ {
+			for char := 'A'; char < 'A'+26; char++ {
+				fmt.Printf("%c ", char)
+			}
+		}
+	}()
+
+	// Wait for the goroutines to finish.
+	fmt.Println("Waiting To Finish")
+	wg.Wait()
+	fmt.Println("\nTerminating Program")
+}
---
+++ concurrency/goroutines_test.go
@@ -0,0 +1,7 @@
+package concurrency
+
+import "testing"
+
+func TestGoroutines(t *testing.T) {
+	Goroutines()
+}
